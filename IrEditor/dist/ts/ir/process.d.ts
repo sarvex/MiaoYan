ec287c91f1d9509ecfbca8b36bd9738b16ed521f
Tw93 <tw93@qq.com>
2022-10-02 21:45:13 +0800

    :sparkles: 即时编辑

--- IrEditor/dist/ts/ir/process.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="./types" />
-export declare const processHint: (vditor: IVditor) => void;
-export declare const processAfterRender: (vditor: IVditor, options?: {
-    enableAddUndoStack: boolean;
-    enableHint: boolean;
-    enableInput: boolean;
-}) => void;
-export declare const processHeading: (vditor: IVditor, value: string) => void;
